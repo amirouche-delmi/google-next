ad4dbb9f5f91d981ef845e3366e8a77ab99c7cde
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-09-28 10:14:08 +0100

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Google Clone",
+  description: "An open source Google clone built with Next.js and Tailwind CSS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
