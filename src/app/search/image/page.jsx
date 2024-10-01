ebc0067a29d314621e1368080f698e16c4652d77
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-10-01 18:08:43 +0100

    update

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,56 +1,31 @@
-'use client'
-
-import { Suspense } from 'react';
 import ImageSearchResults from '@/components/ImageSearchResults';
 import Link from 'next/link';
-import Loading from './loading'; // Importez le composant loading
 
-export default function ImageSearchPage({ searchParams }) {
+export default async function ImageSearchPage({ searchParams }) {
   const startIndex = searchParams.start || '1';
-
-  return (
-    <Suspense fallback={<Loading />}> {/* Utilisez votre composant Loading ici */}
-      <ImageSearchContent searchParams={searchParams} startIndex={startIndex} />
-    </Suspense>
-  );
-}
-
-async function ImageSearchContent({ searchParams, startIndex }) {
-  // Simulating loading state
   await new Promise((resolve) => setTimeout(resolve, 1000));
-
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${encodeURIComponent(searchParams.searchTerm)}&searchType=image&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
-
-  if (!response.ok) {
-    return (
-      <div className='flex flex-col justify-center items-center pt-10'>
-        <h1 className='text-3xl mb-4'>Something went wrong.</h1>
-        <p className='text-lg'>
-          Please try again later or return to{' '}
-          <Link href='/' className='text-blue-500'>Home</Link>.
-        </p>
-      </div>
-    );
-  }
-
+  if (!response.ok) throw new Error('Something went wrong');
   const data = await response.json();
-  const results = data.items || [];
+  const results = data.items;
 
-  if (results.length === 0) {
+  if (!results) {
     return (
       <div className='flex flex-col justify-center items-center pt-10'>
         <h1 className='text-3xl mb-4'>
-          No results found for &quot;{searchParams.searchTerm}&quot;
+          No results found for {searchParams.searchTerm}
         </h1>
         <p className='text-lg'>
           Try searching the web or images for something else{' '}
-          <Link href='/' className='text-blue-500'>Home</Link>.
+          <Link href='/' className='text-blue-500'>
+            Home
+          </Link>
         </p>
       </div>
     );
   }
 
-  return <ImageSearchResults results={results} />;
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
