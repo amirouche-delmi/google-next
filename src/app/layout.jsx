58865b1a92cefcd29568f89db6d3d3150b9e3fd1
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-10-01 15:27:13 +0100

    create search result component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,16 @@
-import Footer from "@/components/Footer";
-import "./globals.css";
+import './globals.css';
+import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: "Google Clone",
-  description: "An open source Google clone built with Next.js and Tailwind CSS",
+  title: 'Google Clone',
+  description:
+    'An open source Google clone built with Next.js and Tailwind CSS.',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang='en'>
+      <body className='relative min-h-screen'>
         {children}
         <Footer />
       </body>
